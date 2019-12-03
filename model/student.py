82fe415f1fb9725a3d7402a59f0a6fec67490245
HyeockJinKim <kherootz@gmail.com>
2019-12-03 21:23:09 +0900

    Add model student

--- model/student.py
+++ model/student.py
@@ -1,7 +1,26 @@
+import os
+
+from git import Repo, GitCommandError
 
 
 class Student:
+    directory_name = 'assignment'
+
     def __init__(self, student_id, name, repo):
         self.id = str(student_id)
         self.name = name
         self.repo = repo
+        self.dir = os.path.join(Student.directory_name, self.id)
+
+    def update_assignment(self):
+        pass
+
+    def clone_repo(self):
+        if os.path.exists(self.dir):
+            return
+
+        os.mkdir(self.dir)
+        try:
+            repo = Repo.clone_from(self.repo, self.dir)
+        except GitCommandError as e:
+            print(self.id, self.name, self.repo, 'repository 문제')
